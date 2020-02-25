a4cf2485a42a2ee2e5092145ae9bccb4a1e9d0ed
Michel Chowanski <michel@chowanski.de>
2020-02-25 11:57:16 +0100

    DEN-219: Make bundle symfony 5 compatible / refactor to controller as a service

--- src/Controller/KitchensinkController.php
+++ src/Controller/KitchensinkController.php
@@ -3,107 +3,87 @@
 namespace BestIt\KitchensinkBundle\Controller;
 
 use BestIt\KitchensinkBundle\DataProviderInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 /**
  * Simple kitchensink controller.
+ *
  * @author blange <lange@bestit-online.de>
  * @package BestIt\KitchensinkBundle
- * @subpackage Controller
- * @version $id$
  */
-class KitchensinkController extends Controller
+class KitchensinkController
 {
     /**
      * The data provider.
-     * @var DataProviderInterface|void
+     *
+     * @var DataProviderInterface
      */
-    private $dataProvider = null;
+    private $dataProvider;
+
+    /**
+     * The template engine
+     *
+     * @var Environment
+     */
+    private $templateEngine;
 
     /**
      * The template name.
+     *
      * @var string
      */
-    private $templateName = '';
+    private $templateName;
 
     /**
-     * Returns the data provider.
-     * @return DataProviderInterface
+     * KitchensinkController constructor.
+     *
+     * @param DataProviderInterface $provider
+     * @param Environment $engine
+     * @param string $template
      */
-    public function getDataProvider(): DataProviderInterface
+    public function __construct(DataProviderInterface $provider, Environment $engine, string $template)
     {
-        if (!$this->dataProvider) {
-            $this->setDataProvider($this->container->get('best_it_kitchensink.data_provider'));
-        }
-
-        return $this->dataProvider;
-    }
-
-    /**
-     * Returns the template name.
-     * @return string
-     */
-    public function getTemplateName(): string
-    {
-        if (!$this->templateName) {
-            $this->setTemplateName($this->container->getParameter('best_it_kitchensink.template'));
-        }
-
-        return $this->templateName;
+        $this->dataProvider = $provider;
+        $this->templateEngine = $engine;
+        $this->templateName = $template;
     }
 
     /**
      * Returns the view data from the data provider.
+     *
      * @return array
      */
-    protected function getViewData(): array
+    private function getViewData(): array
     {
-        $dataProvider = $this->getDataProvider();
         $viewData = [];
 
-        foreach ($dataProvider->getTemplateVars() as $key => $value) {
+        foreach ($this->dataProvider->getTemplateVars() as $key => $value) {
             $withGetter = !is_numeric($key);
 
             $viewData[!$withGetter ? $value : $key] = !$withGetter
-                ? $dataProvider->{'get' . ucfirst($value)}()
-                : $dataProvider->$value();
+                ? $this->dataProvider->{'get' . ucfirst($value)}()
+                : $this->dataProvider->$value();
         }
         return $viewData;
     }
 
     /**
      * Renders the template for the kitchensink.
+     *
+     * @throws LoaderError Error while loading
+     * @throws RuntimeError Runtime errors
+     * @throws SyntaxError Syntax errors
+     *
      * @return Response
      */
     public function indexAction(): Response
     {
-        $viewData = $this->getViewData();
-
-        return $this->render($this->getTemplateName(), $viewData);
-    }
-
-    /**
-     * Sets the data provider.
-     * @param DataProviderInterface $dataProvider
-     * @return KitchensinkController
-     */
-    public function setDataProvider(DataProviderInterface $dataProvider): KitchensinkController
-    {
-        $this->dataProvider = $dataProvider;
-
-        return $this;
-    }
-
-    /**
-     * Sets the template name.
-     * @param string $templateName
-     * @return KitchensinkController
-     */
-    public function setTemplateName(string $templateName): KitchensinkController
-    {
-        $this->templateName = $templateName;
-
-        return $this;
+        return new Response(
+            $this->templateEngine->render($this->templateName, $this->getViewData())
+        );
     }
 }
